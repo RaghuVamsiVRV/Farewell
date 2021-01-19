327d5a254d3173f1e435af477f2599b0a7e3b02d
vsaisujeeth <vsaisujeeth10@yahoo.com>
2021-01-19 23:54:03 +0530

    added backend

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const apiRoutes = require('./routes/api.js');
+const authRoutes = require('./routes/auth');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+
+const requireAuth = (req, res, next) => {
+    try {
+        console.log(req.cookies)
+        const token = req.cookies.jwt
+        // check json web token exists & is verified
+        if (token) {
+            jwt.verify(token, 'secret code', (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.status(400).json({"err":"please login"}); 
+            } else {
+                console.log(decodedToken);
+                next();
+            }
+            });
+        } else {
+            res.status(400).json({"err":"please login"}); 
+        }
+    } catch(err) {
+        console.log(err)
+        res.status(400).json({"err":"please login"}); 
+    }
+  };
+
+  
+mongoose.connect('mongodb+srv://sujeeth:vsaisujeeth@cluster0.84bzl.mongodb.net/mydb',{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true });
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use('/api',requireAuth,apiRoutes);
+app.use(authRoutes);
+
+app.listen(process.env.port || 4000,function(){
+    console.log("now listening for requests");
+});
---
+++ backend/models/users.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+// create users Schema & model
+const UsersSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter an email"],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter a password"],
+    },
+    batch:{
+        type: String
+    },
+    college: {
+        type: String
+    },
+    imageURL:
+    {
+        type:String
+    }
+});
+
+// encrypting the password
+UsersSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
+
+  // static method to login user
+UsersSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+      const auth = await bcrypt.compare(password, user.password);
+      if (auth) {
+        return user;
+      }
+      throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+  };
+
+const Users = mongoose.model('users', UsersSchema);
+
+module.exports = Users;
