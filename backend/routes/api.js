cc4edf7cacd5f843a54ce167ec2968e3b77559b8
vsaisujeeth <vsaisujeeth10@yahoo.com>
2021-01-20 14:00:30 +0530

    added get usr by id

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -29,13 +29,25 @@ router.get('/users', function(req, res){
     }).catch(err=>res.status(400).json({'error': err.message}));
 });
 
-// get a list of comments from the db
-router.get('/comments', function(req, res){
-    let to =req.query.to
-    Comments.find({'to': to}).then(function(comments){
-       res.send({comments});
-    }).catch(err=>res.status(400).json({'error': err.message}));
+router.get('/users/:id', function(req, res){
+     let id = req.params.id;
+    // find document with id 
+    User.findById(id, function (err, docs) { 
+        if (err){ 
+            console.log(err.message); 
+            res.status(400).json({'error':'error in fetching'})
+        } 
+        else{ 
+            if (docs==null){ 
+                res.status(400).json({'error':'Id not found'})
+                return;
+            } 
+            console.log("Result:", docs); 
+            res.status(200).json(docs)
+        } 
+    }); 
 });
+
 
 // get a list of comments of user from the db
 router.get('/my_comments', function(req, res){
@@ -62,7 +74,9 @@ router.put('/edit_comment/:id', function(req, res){
     
     let from = getUserFromToken(req);
     let id = req.params.id;
-
+    let obj= req.body;
+    obj.from = from;
+    obj.time = new Date();
     // find document with id 
     Comments.findById(id, function (err, docs) { 
         if (err){ 
