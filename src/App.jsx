fb70bbcf5d270619f454d9bdd2c6dbb9ccbadd7a
Narendra Nath Gogineni <narenrockstar1@gmail.com>
2021-01-27 15:07:18 +0530

    fetch works now, nothing else changed, added some import statements in main.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,32 @@
-<<<<<<< HEAD
-import song from "./media/bgm.mp3";
-import React, {Component} from 'react';
+import song from './media/bgm.mp3';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Main from './components/main';
-import { Provider } from "react-redux";
-import { ConfigureStore } from "./redux/configureStore";
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
-const store=ConfigureStore();
+
+const store = ConfigureStore();
 
 class App extends Component {
-	render(){
+	render() {
 		return (
 			<Provider store={store}>
 				<BrowserRouter>
-				<audio src={song} autoplay={{}} /*loop={{}}*/>
+					<audio src={song} autoplay={{}} /*loop={{}}*/>
 						{' '}
-						<p>If you are reading this, it is because your browser does not support the audio element</p>{' '}
-				</audio>
+						<p>
+							If you are reading this, it is because your browser does not support the audio element
+						</p>{' '}
+					</audio>
 					<div>
-						<Main/>
+						<Main />
 					</div>
 				</BrowserRouter>
 			</Provider>
 		);
 	}
-=======
-import song from './media/bgm.mp3';
-import React from 'react';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import Main from './components/main';
-import Header from './components/Header';
-
-export default function App() {
-	return (
-		<BrowserRouter>
-			<audio src={song} autoPlay={{}} /*loop={{}}*/>
-				{' '}
-				<p>If you are reading this, it is because your browser does not support the audio element</p>{' '}
-			</audio>
-			<div>
-				<Main />
-			</div>
-		</BrowserRouter>
-	);
->>>>>>> ce7b77fc66ba8729d522097a29e375ffbc3d259b
 }
 
-export default App;	
+export default App;
--- src/components/main.js
+++ src/components/main.js
@@ -1,44 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ProfilePage from '../screens/profilePage';
-import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Bubbles from './bubbles';
-import Header from "./Header";
-import Signup from "./signup";
+import Header from './Header';
+import Signup from './signup';
 import { addComment } from '../redux/ActionCreators';
 import { connect } from 'react-redux';
 
-const mapStateToProps = state => {
-  return {
-    comments: state.comments  
-  }
-}
+const mapStateToProps = (state) => {
+	return {
+		comments: state.comments
+	};
+};
 
-const mapDispatchToProps = dispatch => ({
-  
-  addComment:(id, from, to, comment) => dispatch(addComment(id, from, to, comment))
-
+const mapDispatchToProps = (dispatch) => ({
+	addComment: (id, from, to, comment) => dispatch(addComment(id, from, to, comment))
 });
 
-class Main extends Component{
+class Main extends Component {
+	constructor(props) {
+		super(props);
+	}
 
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-    return(
-      <div> 
-        <Header/>
-        <Switch>
-        <Route path = "/bubbles" component={Bubbles} />
-        <Route exact path = "/profilePage" component={() => <ProfilePage comments={this.props.comments}/>}/>
-        <Route path = "/signup" component={Signup}/>
-        <Redirect to="/bubbles"/>
-        </Switch>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header />
+				<Switch>
+					<Route path="/bubbles" component={Bubbles} />
+					<Route exact path="/profilePage" component={() => <ProfilePage comments={this.props.comments} />} />
+					<Route path="/signup" component={Signup} />
+					<Redirect to="/bubbles" />
+				</Switch>
+			</div>
+		);
+	}
 }
-  
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
