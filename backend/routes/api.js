6c365047bca7f84992543a0abbad87ea1b31662b
vsaisujeeth <vsaisujeeth10@yahoo.com>
2021-02-02 22:10:10 +0530

    comment

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -26,6 +26,15 @@ router.get('/my_comments', function(req, res){
 });
 
 
+// get a list of comments of user from the db
+router.get('/get_comments', function(req, res){
+    let to = req.query.to;
+    Comments.find({'to': to}).then(function(comments){
+       res.send({comments});
+    }).catch(err=>res.status(400).json({'error': err.message}));
+});
+
+
 // add a new comments to the db
 router.post('/add_comment', function(req, res){
     let from = getUserFromToken(req);
