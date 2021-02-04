85659ed3646125b395f7e4e5f27cf15f3bddebad
Rachumallu Yaswant <alextran725@yahoo.com>
2021-02-04 22:51:26 +0530

    final stage 3

--- src/screens/profilePage.js
+++ src/screens/profilePage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Control, LocalForm } from 'react-redux-form';
-<<<<<<< HEAD
 import {Button, Row, Col, Card, CardTitle, CardText, CardBody} from 'reactstrap';
 
 
@@ -21,37 +20,9 @@ function RenderComment({comment}){
         </CardBody>
       </Card>
   );
-=======
-import { Button, Row, Col, Card, CardTitle, CardText } from 'reactstrap';
-import pic from '../media/photos/teja.jpg';
-import REACTGA from 'react-ga';
-
-function RenderComment({ comment }) {
-	return (
-		<Card key={comment.id}>
-			<CardTitle tag="h5">{comment.from}</CardTitle>
-			<CardText>{comment.comment}</CardText>
-			<CardText className="ml-auto mr-3">
-				--{' '}
-				{new Intl.DateTimeFormat('en-US', {
-					day: '2-digit',
-					month: 'short',
-					year: 'numeric'
-				}).format(new Date(comment.date))}
-			</CardText>
-		</Card>
-	);
->>>>>>> 6acc538b61bdebc4e41030a5a99c8599d27b71e8
 }
+
 class ProfilePage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			comments: [],
-			user: {},
-			url: {}
-		};
-	}
 
   constructor(props)
   {
@@ -63,22 +34,12 @@ class ProfilePage extends Component {
     }
     this.handleSubmit=this.handleSubmit.bind(this);
   }
-
-
-  componentDidMount() {
-    console.log(this.props.id);
-	componentWillMount() {
-		import('../media/photos/teja.jpg').then((image) => {
-			this.setState({ url: image });
-		});
-	}
 	componentDidMount() {
 		console.log(this.props.id);
 		fetch(`http://localhost:4000/users/${this.props.id}`)
 			.then((response) => response.json())
-			.then((data) => this.setState({ user: data }));
+			.then((data) => this.setState({user: data}));
 
-<<<<<<< HEAD
     fetch(`http://localhost:4000/get_comments?to=${this.props.id}`)
 			.then((response) => response.json())
       .then((data) => this.setState({comments:data.comments}))
@@ -97,83 +58,26 @@ class ProfilePage extends Component {
       fetch('http://localhost:4000/api/add_comment', requestOptions)
         .then(response => response.json())
         .then(data => this.setState({comments: [...this.state.comments, data]}));
-
-
-      // let comment={
-      //   _id: "600721ebdffb1c45f8af67c4",
-      //   comment: values.comment,
-      //   to: this.state.user._id,
-      //   from: "6007214cf887071e4010bf6d",
-      //   time: "2021-01-19T18:16:11.764Z",
-      //   __v: 0
-      // }
-      // this.setState({
-      //   comments: [...this.state.comments, comment]
-      // })
   }
-  render(){
-    console.log(this.state.comments)
-    const dispComment = this.state.comments.map((comment)=>{
-      return(
-        <Col md={6}>
-          <RenderComment comment={comment}/>
-        </Col>
-      );
-    });
-    return(
-    <div className="container">
-      <div className="container-banner">
-          <img src={this.state.user.imageURL||'/photos/anushree.jpg'} alt="Avatar" height="190" width="190"></img>
-        <h2> {this.state.user.name} </h2>
-        <Row>
-          {dispComment}
-        </Row>
-      </div>
-      <div className="container-banner">
-        <LocalForm onSubmit={this.handleSubmit}>
-          <Row className="form-group">
-            <Col md={12}>
-              <Control.textarea
-                model=".comment"
-                id="comment"
-                name="comment"
-                rows={3}
-                className="form-control"
-              />
-            </Col>
-          </Row>
-          <Button outline type="submit"><span className="fa fa-pencil" /> Submit Comment</Button>
-        </LocalForm>
-      </div>
-    </div>
-    );
-  }
-=======
-		REACTGA.pageview('/' + JSON.stringify(this.state.user));
-	}
+  
 
 	render() {
-		const dispComment = this.props.comments.map((comment) => {
+		const dispComment = this.state.comments.map((comment) => {
 			return (
 				<Col md={6}>
 					<RenderComment comment={comment} />
 				</Col>
 			);
 		});
-		function handleSubmit(values) {
-			console.log('Current State is: ' + JSON.stringify(values));
-			alert('Current State is: ' + JSON.stringify(values));
-			this.props.addComment('nani', 'teja', values.comment);
-		}
 		return (
 			<div className="container">
 				<div className="container-banner">
-					<img src={'/photos/anushree.jpg'} alt="Avatar" height="170" width="170" />
+					<img src={this.state.user.imageURL} alt="Avatar" height="170" width="170" />
 					<h2> {this.state.user.name} </h2>
 					<Row>{dispComment}</Row>
 				</div>
 				<div className="container-banner">
-					<LocalForm onSubmit={(values) => handleSubmit(values)}>
+					<LocalForm onSubmit={this.handleSubmit}>
 						<Row className="form-group">
 							<Col md={12}>
 								<Control.textarea
@@ -191,8 +95,8 @@ class ProfilePage extends Component {
 					</LocalForm>
 				</div>
 			</div>
-		);
+    );
+    // REACTGA.pageview('/' + JSON.stringify(this.state.user));
 	}
->>>>>>> 6acc538b61bdebc4e41030a5a99c8599d27b71e8
 }
 export default ProfilePage;
