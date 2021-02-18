35679adba9ed6f1f6e2893c5026afbdf642a878d
RaghuVamsiVRV <raghuvamsi666666@gmail.com>
2021-02-18 12:06:58 +0530

    profile-update

--- src/screens/profilePage.js
+++ src/screens/profilePage.js
@@ -147,7 +147,7 @@ function RenderComment2({ userB, userB1, comment, onDelete, id, show}) {
 	  }
 	if(show==="2"){
 		setTimeout(function(){
-			Flip(id!=comment.from)
+			Flip(id!==comment.from)
 		}, 250);
 	}
 	else { 
@@ -375,7 +375,7 @@ class ProfilePage extends Component {
 			<div className="lcontainer">
 				<div className="container-banner">
 					<img
-						src={'/photos/' + this.state.user.imageURL || '/photos/anushree.jpg'}
+						src={'/photos/' + this.state.user.imageURL}
 						alt="Avatar"
 						height="170"
 						width="170"
